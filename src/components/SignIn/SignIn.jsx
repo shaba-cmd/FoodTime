de75e8775cab926555a2e35864062e125f91122d
shaba-cmd <global.real@mail.ru>
2026-07-22 20:43:48 +0700

    Изменение репозитория

--- src/components/SignIn/SignIn.jsx
+++
@@ -1,7 +0,0 @@
-import AuthForm from '../AuthForm/AuthForm.jsx'
-
-const SignIn = ({ setIsAuth }) => {
-  return <AuthForm isSignUp={false} setIsAuth={setIsAuth} />
-}
-
-export default SignIn
--- src/components/SignUp/SignUp.jsx
+++
@@ -1,7 +0,0 @@
-import AuthForm from '../AuthForm/AuthForm.jsx'
-
-const SignUp = ({ setIsAuth }) => {
-  return <AuthForm isSignUp={true} setIsAuth={setIsAuth}/>
-}
-
-export default SignUp
